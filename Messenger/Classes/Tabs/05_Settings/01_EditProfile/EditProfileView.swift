df70463879666f2382ab11d045359b9549f7b636
Related Code <info@relatedcode.com>
2018-08-30 18:15:25 +0200

    4.3

---
+++ Messenger/Classes/Tabs/05_Settings/01_EditProfile/EditProfileView.swift
@@ -0,0 +1,321 @@
+//
+// Copyright (c) 2018 Related Code - http://relatedcode.com
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+class EditProfileView: UIViewController, UITableViewDataSource, UITableViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate, CountriesDelegate {
+
+	@IBOutlet var tableView: UITableView!
+	@IBOutlet var viewHeader: UIView!
+	@IBOutlet var imageUser: UIImageView!
+	@IBOutlet var labelInitials: UILabel!
+	@IBOutlet var cellFirstname: UITableViewCell!
+	@IBOutlet var cellLastname: UITableViewCell!
+	@IBOutlet var cellCountry: UITableViewCell!
+	@IBOutlet var cellLocation: UITableViewCell!
+	@IBOutlet var cellPhone: UITableViewCell!
+	@IBOutlet var fieldFirstname: UITextField!
+	@IBOutlet var fieldLastname: UITextField!
+	@IBOutlet var labelPlaceholder: UILabel!
+	@IBOutlet var labelCountry: UILabel!
+	@IBOutlet var fieldLocation: UITextField!
+	@IBOutlet var fieldPhone: UITextField!
+
+	private var isOnboard = false
+
+	//---------------------------------------------------------------------------------------------------------------------------------------------
+	func myInit(isOnboard isOnboard_: Bool) {
+
+		isOnboard = isOnboard_
+	}
+
+	//---------------------------------------------------------------------------------------------------------------------------------------------
+	override func viewDidLoad() {
+
+		super.viewDidLoad()
+		title = "Edit Profile"
+
+		navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(actionCancel))
+		navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(actionDone))
+
+		let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+		tableView.addGestureRecognizer(gestureRecognizer)
+		gestureRecognizer.cancelsTouchesInView = false
+
+		tableView.tableHeaderView = viewHeader
+
+		imageUser.layer.cornerRadius = imageUser.frame.size.width / 2
+		imageUser.layer.masksToBounds = true
+
+		loadUser()
+	}
+
+	//---------------------------------------------------------------------------------------------------------------------------------------------
+	override func viewWillDisappear(_ animated: Bool) {
+
+		super.viewWillDisappear(animated)
+
+		dismissKeyboard()
+	}
+
+	//---------------------------------------------------------------------------------------------------------------------------------------------
+	@objc func dismissKeyboard() {
+
+		view.endEditing(true)
+	}
+
+	// MARK: - Backend actions
+	//---------------------------------------------------------------------------------------------------------------------------------------------
+	func loadUser() {
+
+		let user = FUser.currentUser()
+
+		labelInitials.text = user.initials()
+		if let picture = user[FUSER_PICTURE] as? String {
+			DownloadManager.image(link: picture) { path, error, network in
+				if (error == nil) {
+					self.imageUser.image = UIImage(contentsOfFile: path!)
+					self.labelInitials.text = nil
+				}
+			}
+		}
+
+		fieldFirstname.text = user[FUSER_FIRSTNAME] as? String
+		fieldLastname.text = user[FUSER_LASTNAME] as? String
+
+		labelCountry.text = user[FUSER_COUNTRY] as? String
+		fieldLocation.text = user[FUSER_LOCATION] as? String
+
+		fieldPhone.text = user[FUSER_PHONE] as? String
+
+		let loginMethod = user[FUSER_LOGINMETHOD] as? String
+		fieldPhone.isUserInteractionEnabled = (loginMethod != LOGIN_PHONE)
+
+		updateDetails()
+	}
+
+	//---------------------------------------------------------------------------------------------------------------------------------------------
+	func saveUser() {
+
+		let user = FUser.currentUser()
+
+		let fullname = "\(fieldFirstname.text!) \(fieldLastname.text!)"
+
+		user[FUSER_FIRSTNAME] = fieldFirstname.text!
+		user[FUSER_LASTNAME] = fieldLastname.text!
+		user[FUSER_FULLNAME] = fullname
+		user[FUSER_COUNTRY] = labelCountry.text!
+		user[FUSER_LOCATION] = fieldLocation.text!
+		user[FUSER_PHONE] = fieldPhone.text!
+
+		user.saveInBackground(block: { error in
+			if (error == nil) {
+				Account.update()
+			} else {
+				ProgressHUD.showError("Network error.")
+			}
+		})
+
+		LinkedUser.updateItems()
+	}
+
+	//---------------------------------------------------------------------------------------------------------------------------------------------
+	func saveUserPicture(link: String) {
+
+		let user = FUser.currentUser()
+
+		user[FUSER_PICTURE] = link
+
+		user.saveInBackground(block: { error in
+			if (error == nil) {
+				Account.update()
+			} else {
+				ProgressHUD.showError("Network error.")
+			}
+		})
+
+		LinkedUser.updateItems()
+	}
+
+	//---------------------------------------------------------------------------------------------------------------------------------------------
+	func saveUserThumbnail(link: String) {
+
+		let user = FUser.currentUser()
+
+		user[FUSER_THUMBNAIL] = link
+
+		user.saveInBackground(block: { error in
+			if (error != nil) {
+				ProgressHUD.showError("Network error.")
+			}
+		})
+
+		LinkedUser.updateItems()
+	}
+
+	// MARK: - User actions
+	//---------------------------------------------------------------------------------------------------------------------------------------------
+	@objc func actionCancel() {
+
+		if (isOnboard) {
+			LogoutUser(delAccount: DEL_ACCOUNT_ALL)
+		}
+		dismiss(animated: true)
+	}
+
+	//---------------------------------------------------------------------------------------------------------------------------------------------
+	@objc func actionDone() {
+
+		if (fieldFirstname.text!.count == 0)	{ ProgressHUD.showError("Firstname must be set.");		return		}
+		if (fieldLastname.text!.count == 0)		{ ProgressHUD.showError("Lastname must be set.");		return		}
+		if (labelCountry.text!.count == 0)		{ ProgressHUD.showError("Country must be set.");		return		}
+		if (fieldLocation.text!.count == 0)		{ ProgressHUD.showError("Location must be set.");		return		}
+		if (fieldPhone.text!.count == 0)		{ ProgressHUD.showError("Phone number must be set.");	return		}
+
+		saveUser()
+		dismiss(animated: true)
+	}
+
+	//---------------------------------------------------------------------------------------------------------------------------------------------
+	@IBAction func actionPhoto(_ sender: Any) {
+
+		let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+
+		alert.addAction(UIAlertAction(title: "Open Camera", style: .default, handler: { action in
+			PresentPhotoCamera(target: self, edit: true)
+		}))
+		alert.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { action in
+			PresentPhotoLibrary(target: self, edit: true)
+		}))
+		alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+
+		present(alert, animated: true)
+	}
+
+	//---------------------------------------------------------------------------------------------------------------------------------------------
+	func actionCountries() {
+
+		let countriesView = CountriesView()
+		countriesView.delegate = self
+		let navController = NavigationController(rootViewController: countriesView)
+		present(navController, animated: true)
+	}
+
+	// MARK: - UIImagePickerControllerDelegate
+	//---------------------------------------------------------------------------------------------------------------------------------------------
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+
+		if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+			uploadUserPicture(image: image)
+			uploadUserThumbnail(image: image)
+		}
+
+		picker.dismiss(animated: true)
+	}
+
+	//---------------------------------------------------------------------------------------------------------------------------------------------
+	func uploadUserPicture(image: UIImage) {
+
+		let squared: UIImage = Image.square(image: image, size: 300)
+		if let data = UIImageJPEGRepresentation(image, 0.6) {
+			UploadManager.upload(data: data, name: "profile_picture", ext: "jpg", completion: { link, error in
+				if (error == nil) {
+					self.labelInitials.text = nil
+					self.imageUser.image = squared
+					DownloadManager.saveImage(data: data, link: link!)
+					self.saveUserPicture(link: link!)
+				} else {
+					ProgressHUD.showError("Picture upload error.")
+				}
+			})
+		}
+	}
+
+	//---------------------------------------------------------------------------------------------------------------------------------------------
+	func uploadUserThumbnail(image: UIImage) {
+
+		let squared: UIImage = Image.square(image: image, size: 100)
+		if let data = UIImageJPEGRepresentation(image, 0.6) {
+			UploadManager.upload(data: data, name: "profile_thumbnail", ext: "jpg", completion: { link, error in
+				if (error == nil) {
+					DownloadManager.saveImage(data: data, link: link!)
+					self.saveUserThumbnail(link: link!)
+				} else {
+					ProgressHUD.showError("Thumbnail upload error.")
+				}
+			})
+		}
+	}
+
+	// MARK: - CountriesDelegate
+	//---------------------------------------------------------------------------------------------------------------------------------------------
+	func didSelectCountry(name: String, code: String) {
+
+		labelCountry.text = name
+		fieldLocation.becomeFirstResponder()
+		updateDetails()
+	}
+
+	// MARK: - Table view data source
+	//---------------------------------------------------------------------------------------------------------------------------------------------
+	func numberOfSections(in tableView: UITableView) -> Int {
+
+		return 2
+	}
+
+	//---------------------------------------------------------------------------------------------------------------------------------------------
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+		if (section == 0) { return 4 }
+		if (section == 1) { return 1 }
+
+		return 0
+	}
+
+	//---------------------------------------------------------------------------------------------------------------------------------------------
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+		if (indexPath.section == 0) && (indexPath.row == 0) { return cellFirstname	}
+		if (indexPath.section == 0) && (indexPath.row == 1) { return cellLastname	}
+		if (indexPath.section == 0) && (indexPath.row == 2) { return cellCountry	}
+		if (indexPath.section == 0) && (indexPath.row == 3) { return cellLocation	}
+		if (indexPath.section == 1) && (indexPath.row == 0) { return cellPhone		}
+
+		return UITableViewCell()
+	}
+
+	// MARK: - Table view delegate
+	//---------------------------------------------------------------------------------------------------------------------------------------------
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+		tableView.deselectRow(at: indexPath, animated: true)
+
+		if (indexPath.section == 0) && (indexPath.row == 2) { actionCountries()		}
+	}
+
+	// MARK: - UITextField delegate
+	//---------------------------------------------------------------------------------------------------------------------------------------------
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+
+		if (textField == fieldFirstname)	{ fieldLastname.becomeFirstResponder()	}
+		if (textField == fieldLastname)		{ actionCountries()						}
+		if (textField == fieldLocation)		{ fieldPhone.becomeFirstResponder()		}
+		if (textField == fieldPhone)		{ actionDone()							}
+
+		return true
+	}
+
+	// MARK: - Helper methods
+	//---------------------------------------------------------------------------------------------------------------------------------------------
+	func updateDetails() {
+
+		labelPlaceholder.isHidden = labelCountry.text != nil
+	}
+}
